6316d40d7d13b8d754af32e6d5af3187bea49f8b
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-15 19:19:29 -0300

    se limpio el template y se agrego bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+      <h1>hola mundo</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
