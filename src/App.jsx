453f65b649218ea203ae8d14af9e5a109e74b73a
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-16 22:11:23 -0300

    se corrigio app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Footer from "./components/Footer";
-import FormularioTarea from "./components/FormularioTarea";
 import Navbar from "./components/Navbar";
 import FormularioColor from './components/FormularioColor';
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
-      <h1>hola mundo</h1>
+      <Navbar></Navbar>
+      
+      
+      <main className="container my-5">
+        <h2 className="text-center mb-4 text-secondary">Paleta de Colores</h2>
+        
+        
+        <FormularioColor />
+      </main>
+      
       <Footer />
     </>
   );
