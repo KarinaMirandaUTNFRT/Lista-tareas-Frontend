b9d3072a093e7445110038fbba9ff3a1d91548ef
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-05 15:15:47 -0300

    se maqueto el app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer";
+import Footer from "./components/shared/footer";
 import Navbar from "./components/Navbar";
 import FormularioTarea from './components/FormularioTarea';
 
