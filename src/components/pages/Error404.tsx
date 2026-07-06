f384b709263c776504efc87b2d5ee1c43f6201ae
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-06 20:44:22 -0300

    se agrego la imagen al error 404

--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router';
+import error from '../../assets/error404.jpg'
 const Error404 = () => {
-  return (
-    <div>
-      <p> Pagina no encontrada error 404</p>
-    </div>
-  );
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <img src={error} alt="Error 404" />
+            <p className="text-2xl ">pagina no encontrada 404</p>
+            <Link to={'/'} className='text-blue-400 hover:underline'>Vuelve al inicio</Link>
+        </div>
+    );
 };
 
 export default Error404;
