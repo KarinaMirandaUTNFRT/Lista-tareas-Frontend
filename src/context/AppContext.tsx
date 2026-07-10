7d7082f5077b7d8293ed117a134949b46a319677
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-10 12:09:13 -0300

    se corrigio el componente context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,14 +1,11 @@
 import { createContext, useContext } from "react";
-import type { Tarea, TareaFormData } from "../interfaces/tareas";
+import type { Servicio, ServicioFormData } from "../interfaces/servicios";
 
 export interface AppContextType {
   usuarioLogueado: boolean;
   setUsuarioLogueado: React.Dispatch<React.SetStateAction<boolean>>;
-  tareas: Tarea[];
-  crearTarea: (nuevoTarea: TareaFormData) => void;
-  borrarTarea: (idTarea: string) => void;
-  editarTarea: (idTarea: string, tareaEditar: TareaFormData) => void;
-  buscarTarea: (idTarea: string) => Tarea | undefined;
+  servicios: Servicio[];
+  crearServicio: (nuevoServicio: ServicioFormData) => void;
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
